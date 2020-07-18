dc6227ee3c5aa2a39c6dec0a8109c933288e7dde
erioni s <comebellalacitta@hotmail.com>
2020-07-18 10:02:28 +0200

    Revert "added again text"
    This reverts commit c1b2290ad10520ad0704fa1cfa89555678219f02.

--- GitHubConsole/Program.cs
+++ GitHubConsole/Program.cs
@@ -10,7 +10,7 @@ namespace GitHubConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("add text again amd again");
+            Console.WriteLine("add text again");
             Console.ReadLine();
         }
     }
