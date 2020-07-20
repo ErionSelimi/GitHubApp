de5492e8605e30d53f16344200024ea191a659f4
erioni s <comebellalacitta@hotmail.com>
2020-07-20 12:06:34 +0200

    nono

--- GitHubConsole/Person.cs
+++ GitHubConsole/Person.cs
@@ -11,6 +11,6 @@ namespace GitHubConsole
         public int idPerson { get; set; }
         public string Name { get; set; }
 
-        public int No { get; set; }
+        public int NoNo { get; set; }
     }
 }
